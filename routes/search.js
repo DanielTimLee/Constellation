68a62192f0b8bc833e92809b4c9e93ecfb2308af
Daniel T. Lee <danieltimlee@gmail.com>
2018-09-22 21:17:11 +0900

    Router: Search validator enhanced. limit result param added
    Signed-off-by: Daniel T. Lee <danieltimlee@gmail.com>

--- routes/search.js
+++ routes/search.js
@@ -1,20 +1,23 @@
 const express = require('express');
+const {check} = require('express-validator/check');
 const router = express.Router();
 const models = require('../db/models');
 
-router.get('/', (req, res) => {
-  req.checkQuery('name', 'Invalid Query').isLength({min: 2});
-
+router.get('/name', [
+  check('name', 'Invalid Query').isLength({min: 2}),
+  check('limit', 'Invalid limit').optional().isInt({lt: 999}).toInt()
+], (req, res) => {
   const err = req.validationErrors();
   if (err) {
     res.status(401).json(err);
   } else {
-    models.store.findAll(
-      {
-        limit: 10,
-        where: {name: {[models.Sequelize.Op.like]: `%${req.query.name}%`}}
-      })
-      .then(row => res.status(200).json(row));
+    const {name} = req.query;
+    const op = models.Sequelize.Op;
+
+    models.store.findAll({
+      limit: req.query.limit || 10,
+      where: {name: {[op.like]: `%${name}%`}}
+    }).then(row => res.status(200).json(row));
   }
 });
 
