1c346fee0b3443d5db88c39a8d3733f385327eef
Daniel T. Lee <danieltimlee@gmail.com>
2018-09-22 21:17:33 +0900

    Router: Near 3km Distance API Implemented
    Signed-off-by: Daniel T. Lee <danieltimlee@gmail.com>

--- routes/search.js
+++ routes/search.js
@@ -21,5 +21,29 @@ router.get('/name', [
   }
 });
 
+router.get('/near', [
+  check('x', 'Invalid location').isFloat({gt: 124, lt: 132}).toFloat(),
+  check('y', 'Invalid location').isFloat({gt: 33, lt: 43}).toFloat(),
+  check('limit', 'Invalid limit').optional().isInt({lt: 999}).toInt()
+], (req, res) => {
+  const err = req.validationErrors();
+  if (err) {
+    res.status(401).json(err);
+  } else {
+    const {x: lng, y: lat} = req.query;
+    const op = models.Sequelize.Op;
+
+    models.store.findAll({
+      limit: req.query.limit || 10,
+      where: {
+        [op.and]: {
+          x: {[op.between]: [lng - 0.035, lng + 0.035]},
+          y: {[op.between]: [lat - 0.027, lat + 0.027]}
+        }
+      }
+    }).then(row => res.status(200).json(row));
+  }
+});
+
 module.exports = router;
 
